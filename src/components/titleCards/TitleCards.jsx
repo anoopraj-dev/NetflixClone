6c87a284752087b175457205d824a747de6d0583
Anoop Raj <anoopraj.dev@gmail.com>
2025-08-09 02:19:59 +0530

    Before adding tmdb api

--- src/components/titleCards/TitleCards.jsx
+++ src/components/titleCards/TitleCards.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import './TitleCards.css'
+import cardsData from '../../assets/cards/Cards_data'
 
-const TitleCards = () => {
+
+const TitleCards = ({title,category}) => {
+  const [apiData,setApiData] =useState([])
+  const cardsRef = useRef();
+
+  
+  const handleWheel = (event) =>{
+    event.preventDefault();
+    cardsRef.current.scrollLeft +=event.deltaY;
+  }
+
+  useEffect(() => {
+    cardsRef.current.addEventListener('wheel', handleWheel)
+  },[])
+
   return (
-    <div className='titlecards'>
+    <div className='title-cards'>
+      <h2>{title ? title:'Popular on Netflix'}</h2>
+      <div className="card-list" ref={cardsRef}>
+          {
+            cardsData.map((card,index)=>{
+              return <div className="card" key={index}>
+                  <img src={card.image} alt="" />
+                  <p>{card.name}</p>
+              </div>
+            })
+          }
+      </div>
       
     </div>
   )
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,17 +3,34 @@ import './Home.css'
 import Navbar from '../../components/navbar/Navbar'
 import heroBanner from '../../assets/hero_banner.jpg'
 import heroTitle from '../../assets/hero_title.png'
+import playIcon from '../../assets/play_icon.png'
+import infoIcon from '../../assets/info_icon.png'
+import TitleCards from '../../components/titleCards/TitleCards'
+import Footer from '../../components/footer/Footer'
 const Home = () => {
   return (
     <div className='home'>
-      <Navbar/>
+      <Navbar />
       <div className="hero">
         <img src={heroBanner} alt="" className='banner-img' />
         <div className="hero-caption">
-          <img src={heroTitle} alt="" className='caption-img'/>
+          <img src={heroTitle} alt="" className='caption-img' />
           <p>Discovering his ties to a secret ancient order, a young man living in modern Istanbul embarks on a quest to save the city from an immortal enemy</p>
+          <div className="hero-btns">
+            <button className='btn'><img src={playIcon} alt="" />Play</button>
+            <button className='btn dark-btn'><img src={infoIcon} alt="" />More Info</button>
+
+          </div>
+          <TitleCards/>
         </div>
       </div>
+      <div className="more-cards">
+          <TitleCards title={'Blockbuster Movies'}/>
+          <TitleCards title={'Only on Netflix'}/>
+          <TitleCards title={'Upcoming'}/>
+          <TitleCards title={'Top Picks for You'}/>
+      </div>
+      <Footer/>
     </div>
   )
 }
