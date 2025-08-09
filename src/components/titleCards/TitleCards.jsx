91931a198da7dd631d0505c5c78c01b75ae18bd2
Anoop Raj <anoopraj.dev@gmail.com>
2025-08-09 17:55:02 +0530

    Added api call to tmdb

--- src/components/titleCards/TitleCards.jsx
+++ src/components/titleCards/TitleCards.jsx
@@ -3,34 +3,64 @@ import './TitleCards.css'
 import cardsData from '../../assets/cards/Cards_data'
 
 
-const TitleCards = ({title,category}) => {
-  const [apiData,setApiData] =useState([])
+
+const TitleCards = ({ title, category }) => {
+
+  const [apiData, setApiData] = useState([]);
+  const [errorMsg, setErrorMsg] = useState(null);
   const cardsRef = useRef();
 
-  
-  const handleWheel = (event) =>{
+
+  const API_URL = import.meta.env.VITE_TMDB_API_URL;
+  const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+  const IMG_BASE_URL = import.meta.env.VITE_TMDB_IMG_BASE_URL;
+  const endPoint = `${API_URL}${category ? category : 'now_playing'}${API_KEY}`
+
+  const handleWheel = (event) => {
     event.preventDefault();
-    cardsRef.current.scrollLeft +=event.deltaY;
+    cardsRef.current.scrollLeft += event.deltaY;
+  }
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch(endPoint);
+
+      if (!response.ok) {
+        throw new Error(`HTTP error: ${response.status}`);
+      }
+
+      const data = await response.json();
+      setApiData(data.results);
+    } catch (error) {
+      console.error('fetch failed', error);
+      setErrorMsg("⚠️ Unable to load movies. Please try again later.");
+    }
   }
 
   useEffect(() => {
+    fetchData();
     cardsRef.current.addEventListener('wheel', handleWheel)
-  },[])
+  }, [])
 
   return (
     <div className='title-cards'>
-      <h2>{title ? title:'Popular on Netflix'}</h2>
-      <div className="card-list" ref={cardsRef}>
-          {
-            cardsData.map((card,index)=>{
-              return <div className="card" key={index}>
-                  <img src={card.image} alt="" />
-                  <p>{card.name}</p>
-              </div>
-            })
-          }
-      </div>
-      
+      <h2>{title ? title : 'Popular on Netflix'}</h2>
+      {
+        errorMsg ? (
+          <div className="error-message">{errorMsg}</div>
+        ) :
+          <div className="card-list" ref={cardsRef}>
+            {
+              apiData.map((card, index) => {
+                return <div className="card" key={index}>
+                  <img src={IMG_BASE_URL+card.backdrop_path} alt="" />
+                  <p>{card.original_title}</p>
+                </div>
+              })
+            }
+          </div>
+      }
+
     </div>
   )
 }
--- src/pages/player/Player.jsx
+++ src/pages/player/Player.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import './Player.css'
+import backArrow from '../../assets/back_arrow_icon.png'
 
 const Player = () => {
   return (
     <div className='player'>
+
+      <img src={backArrow} alt="" />
+      <iframe width='90%' height ='90%' src="" frameborder="0"></iframe>
       
     </div>
   )
